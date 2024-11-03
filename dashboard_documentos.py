cee1468d426ded6c6df4b16d66c38036db9c76f9
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-11-03 15:48:20 -0300

    Update dashboard_documentos.py

--- dashboard_documentos.py
+++ dashboard_documentos.py
@@ -48,8 +48,8 @@ def show_dashboard():
     )
     st.plotly_chart(fig_tipo_documento)
 
-    title="Distribuição dos documentos por assentamento"
     assentamento_data = df_pgt['Assentamento'].value_counts()
+    title="Distribuição dos documentos por assentamento" 
     st.bar_chart(assentamento_data)
 
     if 'Objetivo' in df_pgt.columns:
