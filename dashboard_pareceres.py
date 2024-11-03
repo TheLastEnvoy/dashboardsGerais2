2ae1fb6d6af3592e700f9caff372bebf9ea32470
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-11-03 16:03:14 -0300

    Update dashboard_pareceres.py

--- dashboard_pareceres.py
+++ dashboard_pareceres.py
@@ -8,7 +8,7 @@ def load_data(sheet_name):
     return pd.read_excel('contPGT_contPlanilhas.xlsx', sheet_name=sheet_name)
 
 def show_dashboard():
-    st.header("Dashboard de Pareceres")
+    st.header("Pareceres Conclusivos")
     df_pareceres = load_data(sheet_name='contPareceres')
 
     assentamentos = ['Todos'] + sorted(list(df_pareceres['Assentamento'].unique()))
@@ -53,7 +53,7 @@ def show_dashboard():
     ))
     fig_progress_pareceres.update_layout(
         barmode='stack',
-        title='Progresso dos Pareceres',
+    st.header("Progresso dos pareceres")
         xaxis_title='Status',
         yaxis_title='Quantidade',
         legend_title='Legenda'
