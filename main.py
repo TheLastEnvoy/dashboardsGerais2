c64e790eae74c059923a4bf78eb4879e5817b904
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-11-03 19:15:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dashboard_laudos import show_dashboard as show_dashboard_laudos
 
 def main():
     st.title("Dashboard Supervisão Ocupacional - TED INCRA/UFPR")
+
+    # Criação das abas
     tab1, tab2, tab3, tab4 = st.tabs([
         "Documentação PGT",
         "Planilhas monitoramento",
@@ -13,16 +15,25 @@ def main():
         "Laudos de supervisão ocupacional"
     ])
 
+    # Configuração do menu lateral para cada aba
     with tab1:
+        st.sidebar.title("Filtros - Documentação PGT")
+        # Adicione aqui os filtros específicos para o dashboard de Documentação PGT
         show_dashboard_documentos()
 
     with tab2:
+        st.sidebar.title("Filtros - Planilhas monitoramento")
+        # Adicione aqui os filtros específicos para o dashboard de Planilhas monitoramento
         show_dashboard_planilhas()
 
     with tab3:
+        st.sidebar.title("Filtros - Pareceres conclusivos")
+        # Adicione aqui os filtros específicos para o dashboard de Pareceres conclusivos
         show_dashboard_pareceres()
 
     with tab4:
+        st.sidebar.title("Filtros - Laudos de supervisão ocupacional")
+        # Adicione aqui os filtros específicos para o dashboard de Laudos de supervisão ocupacional
         show_dashboard_laudos()
 
 if __name__ == "__main__":
