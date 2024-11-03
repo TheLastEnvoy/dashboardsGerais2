1d77cc518a3de466690387cd7f36517ff85a1ef4
the last envoy <81522064+TheLastEnvoy@users.noreply.github.com>
2024-11-03 15:46:24 -0300

    Update dashboard_documentos.py

--- dashboard_documentos.py
+++ dashboard_documentos.py
@@ -50,8 +50,8 @@ def show_dashboard():
 
     st.subheader("Distribuição dos documentos por assentamento")
     assentamento_data = df_pgt['Assentamento'].value_counts()
-    title='Distribuição dos documentos por assentamento'
     st.bar_chart(assentamento_data)
+    title='Distribuição dos documentos por assentamento'
 
     if 'Objetivo' in df_pgt.columns:
         st.subheader("Distribuição dos documentos por objetivo")
